eb26b2417c030af0b36df4545e310859892f72e0
Hillary Tanui <hillarytanui80@gmail.com>
2025-11-30 13:13:34 +0300

    completed assignment

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,13 @@
 # your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_addresses):
+        self.email_addresses = email_addresses
+
+    def parse(self):
+       
+        filter_string = re.compile(r'[A-z]\w*\.\w+@\w+\.[a-z]+|[A-z]\w*\@\w+\.[a-z]+')
+        filtered_emails = filter_string.findall(self.email_addresses)
+        unique_emails = sorted(set(filtered_emails))
+        return unique_emails
